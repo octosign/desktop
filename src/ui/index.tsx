1c0446f618b0eb43880ff04851a9284e9b3411f6
Jakub Ďuraš <jakub@duras.me>
2020-04-27 15:19:36 +0200

    Remove i18next setup duplication

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -5,7 +5,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './components/App';
 import mockWindowAPI from './mockWindowAPI';
 import Providers from './components/Providers';
-import '../shared/i18nSetup';
 
 // Mock window APIs if they were not loaded from electron - during UI development/UI tests
 if (process.env.NODE_ENV === 'development' && window.OctoSign === undefined) {
